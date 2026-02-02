9f5a43ed5410d002ae87122a7c9279d28819c4d8
Jeremi Levesque <jeremilevesque@hotmail.com>
2026-02-02 15:03:25 -0500

    Add functionnalities to plotjson class

--- src/clinical_combat/utils/plotjson.py
+++ src/clinical_combat/utils/plotjson.py
@@ -1,7 +1,7 @@
 """
 Mostly used to visualize the harmonization results simply in
-QC reports, we provide this class which saves inputs of plots
-in a single JSON file.
+QC reports, we provide this class which saves/load inputs of
+plots in a single JSON file.
 """
 
 import json
@@ -18,7 +18,7 @@ class PlotJson:
         self.metric = metric
         self.data = {}
 
-    def add_plot(self, plot_name: str, plot_class: Type, plot_group: str = None, **kwargs):
+    def add_plot(self, plot_name, plot_class: Type, **kwargs):
         """Add data for a specific plot.
 
         Args:
@@ -26,13 +26,7 @@ class PlotJson:
             plot_data (dict): The data associated with the plot.
         """
         kwargs['plot_class'] = plot_class.value
-
-        if plot_group is not None:
-            if plot_group not in self.data:
-                self.data[plot_group] = {}
-            self.data[plot_group][plot_name] = kwargs
-        else:
-            self.data[plot_name] = kwargs
+        self.data[plot_name] = kwargs
 
     def to_json(self):
         """Convert the stored plot data to a JSON-compatible dictionary.
@@ -54,11 +48,29 @@ class PlotJson:
 class PlotJsonAggregator:
     def __init__(self):
         self.aggregated_data = defaultdict(dict)
+
+    @property
+    def data(self):
+        return self.aggregated_data
     
     @classmethod
     def from_file(cls, filepath):
         aggregator = cls()
         aggregator.add_plot_json_aggregator_from_file(filepath)
+        return aggregator
+    
+    @classmethod
+    def from_json(cls, json_data: str):
+        aggregator = cls()
+
+        if not isinstance(json_data, list):
+            json_data = [json_data]
+
+        for j in json_data:
+            data = json.loads(j)
+            for bundle, metrics in data.items():
+                for metric, plots in metrics.items():
+                    aggregator.aggregated_data[bundle][metric] = plots
         return aggregator
 
     def add_plot_json(self, plot_json: PlotJson):
